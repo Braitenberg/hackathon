af6916fa5c3c2b7bbd30d67a4f2da049a5ee7cf4
Amin <amin@Amins-MacBook-Pro-2.local>
2019-11-29 15:55:23 +0100

    sending feedback respons in mail

--- mail/mail.py
+++ mail/mail.py
@@ -1,2 +1,36 @@
-class Model:
+import smtplib
+
+
+class Mail:
+
+    def mail(message):
+        email =["y.dian@live.nl", "theaminaloui@hotmail.com", "" ]
+
+        try:
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.ehlo()
+            server.starttls()
+            server.ehlo()
+
+            server.login('python.novi@gmail.com', 'V=7^GGPh^;gF\,P*')
+
+            subject = 'Feedback'
+            body = message
+
+            msg = f"Subject: {subject}\n\n{body}"
+
+            server.sendmail(
+                'python.novi@gmail.com',
+                email,
+                msg.encode("utf8")
+            )
+
+
+        except Exception as e:
+            return "Mail versturen is mislukt"
+
+        server.quit()
+
+    mail("message")
+
 
