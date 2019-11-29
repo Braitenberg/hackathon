203f61ff2af031063ddc6c45555be96bb4d4f28c
braitenberg <tristankjeld@gmail.com>
2019-11-29 15:53:29 +0100

    added text recognition

--- static/script.js
+++ static/script.js
@@ -4,4 +4,11 @@ recognition.continuous = true;
 var output = document.getElementById('output');
 recognition.onresult = function(event) {
   output.textContent = event.results[0][0].transcript;
+  sendmail(event.results[0][0].transcript)
 };
+
+function sendmail(txt) {
+	var request = new XMLHttpRequest()
+	request.open('GET', 'localhost/sendmail?mail=', true)
+	request.send()
+}
